a1bd7e409c5decccd302e541a8ca58bf52b774f7
unknown <desaiabhijit28@gmail.com>
2024-04-01 21:51:20 +0530

    toDo

---
+++ spring-hello-world/src/main/java/com/abhijit/springhelloworld/HelloController.java
@@ -0,0 +1,12 @@
+package com.abhijit.springhelloworld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+   @RequestMapping("/hello")
+    public String hello(){
+        return "hello spring boot!!!";
+    }
+}
