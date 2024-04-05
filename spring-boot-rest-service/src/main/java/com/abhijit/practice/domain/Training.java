ef2a0898d5c35ebe1a1b8c5ecc9636b6382c93bb
unknown <desaiabhijit28@gmail.com>
2024-04-05 21:20:16 +0530

    springBoot rest service

---
+++ spring-boot-rest-service/src/main/java/com/abhijit/practice/domain/Training.java
@@ -0,0 +1,16 @@
+package com.abhijit.practice.domain;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class Training {
+    private int id;
+    private String name;
+    private int duration;
+    private String prerequisite;
+}
