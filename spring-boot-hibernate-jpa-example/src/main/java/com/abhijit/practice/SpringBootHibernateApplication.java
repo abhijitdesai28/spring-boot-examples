0985954668965b9bc56c7441572c075322aff153
unknown <desaiabhijit28@gmail.com>
2024-04-27 20:40:46 +0530

    hibernate jpa example

---
+++ spring-boot-hibernate-jpa-example/src/main/java/com/abhijit/practice/SpringBootHibernateApplication.java
@@ -0,0 +1,14 @@
+package com.abhijit.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class SpringBootHibernateApplication
+{
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHibernateApplication.class, args);
+	}
+}
---
+++ spring-boot-hibernate-jpa-example/src/main/java/com/abhijit/practice/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.abhijit.practice.repository;
+
+import com.abhijit.practice.domain.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+}
